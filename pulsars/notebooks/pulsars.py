944e8f4e16b8bee43c284dd167cdc8b08f9886da
Aleksei Zhuvikin <zhuvikin@gmail.com>
2022-11-19 19:35:40 +0100

    Use jupytext tool

---
+++ pulsars/notebooks/pulsars.py
@@ -0,0 +1,18 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.4'
+#       jupytext_version: 1.2.1
+#   kernelspec:
+#     display_name: JKERNEL
+#     language: python
+#     name: jkernel
+# ---
+
+# Pulsars
+
+
